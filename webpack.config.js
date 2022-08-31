ef679cf705fd8d5efe8351a9757100e316fea3de
d1mag1c <106352353+d1mag1c@users.noreply.github.com>
2022-09-01 01:07:16 +0300

    webpack_first_config

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require("path");
+module.exports = {
+    mode: 'development',
+    entry: './src/script/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: "main.js"
+    }
+}
