266ca580e2775d5e0680f20bb453de5ba13719a5
d1mag1c <106352353+d1mag1c@users.noreply.github.com>
2022-09-04 23:18:17 +0300

    burger commit 1

--- src/script/burger_menu.js
+++ src/script/burger_menu.js
@@ -1,5 +1,20 @@
-const buttonBurger = document.querySelector('.burger-btn');
+const buttonBurger = document.querySelector('.nav-elem');
+const burgerMenu = document.querySelector('.burger');
+const body = document.querySelector('body');
 
 buttonBurger.addEventListener('click', function (){
+    burgerMenu.classList.remove('burger_trans')
+    body.classList.add('body-overflow')
 
+    const overlayBurger = document.createElement('div')
+    burgerMenu.after(overlayBurger)
+    overlayBurger.classList.add('overlay_burger')
+
+    const buttonBurgerClose = document.querySelector('.burger__close');
+    buttonBurgerClose.addEventListener('click', function (){
+        burgerMenu.classList.add('burger_trans');
+        body.classList.remove('body-overflow')
+        overlayBurger.remove()
+    })
 })
+
