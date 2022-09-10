edca64e2f292ac1ea324da37aff11a43bed2638e
d1mag1c <106352353+d1mag1c@users.noreply.github.com>
2022-09-10 14:49:46 +0300

    fix burger

--- src/script/burger_menu.js
+++ src/script/burger_menu.js
@@ -3,50 +3,40 @@ import {blockCard, lowblockCard} from "./cards";
 const buttonBurger = document.querySelector('.nav-elem');
 const burgerMenu = document.querySelector('.burger');
 const body = document.querySelector('body');
-const overlayBurger = document.createElement('div')
+const overlayBurger = document.createElement('div');
 
 function closeMenu() {
     burgerMenu.classList.add('burger_trans');
-    body.classList.remove('body-overflow')
-    overlayBurger.remove()
+    body.classList.remove('body-overflow');
+    overlayBurger.remove();
 }
 
 buttonBurger.addEventListener('click', function () {
 
-    burgerMenu.classList.remove('burger_trans')
-    body.classList.add('body-overflow')
-    burgerMenu.after(overlayBurger)
-    overlayBurger.classList.add('overlay_burger')
-
+    burgerMenu.classList.remove('burger_trans');
+    body.classList.add('body-overflow');
+    burgerMenu.after(overlayBurger);
+    overlayBurger.classList.add('overlay_burger');
 })
 
-const buttonBurgerClose = document.querySelector('.burger__close');
-buttonBurgerClose.addEventListener('click', closeMenu)
+document.querySelector('.burger__close').addEventListener('click', closeMenu);
 
 body.addEventListener('click', function (evt) {
-    if (evt.target.className === 'overlay_burger') {
-        closeMenu()
-    }
+
+    if (evt.target.className === 'overlay_burger') closeMenu();
 })
 
-const burgerUl = document.querySelector('.burger__list');
-const li = burgerUl.querySelectorAll('li')
-burgerUl.addEventListener('click', function (evt){
+document.querySelector('.burger__list').addEventListener('click', function (evt){
 
-    for(let i of li){
-        if (evt.target === i) {
-            closeMenu()
-        }
-    }
+        if (evt.target.closest('li')) closeMenu();
 })
 
 function burgerSort(evt, cardsArray) {
     const liClick = evt.target.closest('li');
 
     if (liClick) {
-        debugger
         let filterCards = cardsArray.filter((card) => card.category === liClick.textContent);
-        blockCard(filterCards)
+        blockCard(filterCards);
         lowblockCard(filterCards);
     }
 }
