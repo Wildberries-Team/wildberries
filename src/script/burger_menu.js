c68015ea675af61d5273144ccec3002bf42f0aee
d1mag1c <106352353+d1mag1c@users.noreply.github.com>
2022-09-16 15:37:22 +0300

    fix

--- src/script/burger_menu.js
+++ src/script/burger_menu.js
@@ -1,15 +1,19 @@
-import {blockCard, lowblockCard} from "./cards";
+import {blockCard} from "./cards";
 
 const buttonBurger = document.querySelector('.nav-elem');
 const burgerMenu = document.querySelector('.burger');
 const body = document.querySelector('body');
 const overlayBurger = document.querySelector('.black_burger');
 
+// функция закрытия бургера
+
 function closeMenu() {
     burgerMenu.classList.add('burger_trans');
     body.classList.remove('body-overflow');
     overlayBurger.classList.remove('overlay_burger');
 }
+
+// открытие бургера
 
 buttonBurger.addEventListener('click', function () {
 
@@ -18,17 +22,16 @@ buttonBurger.addEventListener('click', function () {
     overlayBurger.classList.add('overlay_burger');
 })
 
+// события закрытия бургера
+
+overlayBurger.addEventListener('click', closeMenu);
 document.querySelector('.burger__close').addEventListener('click', closeMenu);
-
-body.addEventListener('click', function (evt) {
-
-    if (evt.target.className === 'overlay_burger') closeMenu();
-})
-
 document.querySelector('.burger__list').addEventListener('click', function (evt) {
 
     if (evt.target.closest('li')) closeMenu();
 })
+
+// фильтр карточек по категории, убирает слайдер, добавляет кнопку "Убрать фильтр"
 
 function burgerSort(evt, cardsArray) {
     const liClick = evt.target.closest('li');
@@ -38,13 +41,18 @@ function burgerSort(evt, cardsArray) {
         blockCard(filterCards);
         document.querySelector('.main__banner').classList.add('display_none');
         document.querySelector('.container__remove_filter').classList.remove('display_none');
+        document.querySelector('.btn-show-more').classList.add('display_none');
+
     }
 
 }
 
+// добавляет слайдер, убирает кнопку "Убрать фильтр"
+
 function removeFilerBurger(){
     document.querySelector('.main__banner').classList.remove('display_none');
     document.querySelector('.container__remove_filter').classList.add('display_none');
+    document.querySelector('.btn-show-more').classList.remove('display_none');
 }
 
 export {burgerSort, removeFilerBurger}
