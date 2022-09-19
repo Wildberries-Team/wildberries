54982a239ac78f7dce1a27089b88d9ff075209a5
julia_sankevich <sherstnevajulias@mail.ru>
2022-09-19 22:36:52 +0300

    add form log-in

--- src/script/index.js
+++ src/script/index.js
@@ -85,6 +85,31 @@ document.getElementById('basket-btn').addEventListener("click", () => {
     document.querySelector(".background-color-container").style.display = "block";
 });
 
+//открытие меню авторизации
+document.getElementById('log-in__btn').addEventListener("click", () => {
+    document.querySelector(".log-in__container").style.display = "block";
+});
+
+//закрытие меню авторизации на X
+document.querySelector(".log-in__close").addEventListener("click", () => {
+    document.querySelector(".log-in__container").style.display = "none";
+});
+
+//закрытие меню авторизации вне контейнера
+document.addEventListener('click', (e) => {
+    if(e.target.classList == "log-in__container") {
+        document.querySelector(".log-in__container").style.display = "none";
+    };
+});
+
+//закрытие окна авторизации при нажатии на кнопку войт и удаление введенных данных
+document.querySelector(".log-in__button").addEventListener("click", () => {
+    document.querySelector(".log-in__container").style.display = "none";
+    document.querySelector(".log-in__email").value = "";
+    document.querySelector(".log-in__password").value = "";
+
+});
+
 //закрытие корзины вне контейнера
 document.addEventListener('click', (e) => {
     if(e.target.classList == "background-color-container") {
